b39bd451eee6f323415bb8f7f8824f33f24c926b
qhuy0801 <qhuy0168@gmail.com>
2024-04-12 15:13:41 +0100

    :recycle: Update function to process the timeframe_v2

--- application/main/utils/timeframe_processing.py
+++ application/main/utils/timeframe_processing.py
@@ -1,28 +1,38 @@
 """
 The module for processing the trading timeframe
 """
-from typing import List
+from typing import List, Optional
 
 import pandas as pd
 
 
 def process_timeframe(
-    dataframes: List[pd.DataFrame], index_column: str = "timestamp"
+    dataframes: List[pd.DataFrame],
+    index_column: str = "timestamp",
+    former_index_column: Optional[str] = "id",
 ) -> List[pd.DataFrame]:
     """
 
     Args:
         dataframes (List[pd.DataFrame]): List of DataFrames with index in "YYYY-MM-DD" format.
         index_column (str): Name of the column to use as the index, default 'timestamp'.
+        former_index_column (Optional[str]): The name of previous index column to be dropped.
 
     Returns:
         List[pd.DataFrame]: List of DataFrames cropped to the common timeframe with matched indices.
     """
 
+    if len(dataframes) == 1:
+        df = dataframes[0].set_index(index_column)
+        df.index = pd.to_datetime(df.index)
+        df.drop(columns=[former_index_column], inplace=True)
+        return [df]
+
     # Set indices to the specified column and convert to datetime
     for i in range(len(dataframes)):
         dataframes[i] = dataframes[i].set_index(index_column)
         dataframes[i].index = pd.to_datetime(dataframes[i].index)
+        dataframes[i].drop(columns=[former_index_column], inplace=True)
 
     # Determine the common timeframe across all dataframes
     common_start = max(df.index.min() for df in dataframes)
