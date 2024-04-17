50fe3369322af65a5dd78543410d0ccc9daafaf2
qhuy0801 <qhuy0168@gmail.com>
2024-04-17 15:49:00 +0100

    :ambulance: Indicators entity

---
+++ application/main/database/entities/indicators.py
@@ -0,0 +1,43 @@
+import warnings
+from sqlalchemy import exc as sa_exc
+from sqlalchemy import Integer, Column, DateTime, Float
+
+
+def macd_tick(Base, table_name):
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", category=sa_exc.SAWarning)
+
+        class MacdTick(Base):
+            __tablename__ = f"{table_name}"
+            id = Column(Integer, primary_key=True)
+            time = Column(DateTime, unique=True)
+            macd = Column(Float)
+            macd_hist = Column(Float)
+            macd_signal = Column(Float)
+    return MacdTick
+
+
+def bbands_ticks(Base, table_name):
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", category=sa_exc.SAWarning)
+
+        class BBandsTick(Base):
+            __tablename__ = f"{table_name}"
+            id = Column(Integer, primary_key=True)
+            time = Column(DateTime, unique=True)
+            real_lower_band = Column(Float)
+            real_middle_band = Column(Float)
+            real_upper_band = Column(Float)
+    return BBandsTick
+
+
+def rsi_ticks(Base, table_name):
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", category=sa_exc.SAWarning)
+
+        class RSITick(Base):
+            __tablename__ = f"{table_name}"
+            id = Column(Integer, primary_key=True)
+            time = Column(DateTime, unique=True)
+            rsi = Column(Float)
+    return RSITick
