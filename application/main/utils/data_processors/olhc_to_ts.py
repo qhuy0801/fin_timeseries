1bd4ca93712691fdc23b8707fa4b4d58d5c978c1
qhuy0801 <qhuy0168@gmail.com>
2024-04-22 17:07:56 +0100

    :memo: Update new ts processor to adapt with new changes in indicator

--- application/main/utils/data_processors/olhc_to_ts.py
+++ application/main/utils/data_processors/olhc_to_ts.py
@@ -4,13 +4,13 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-from timeframe_processing import process_timeframe
+from .timeframe_processing import process_timeframe
 
 
 def trend_ts(
     df_target_asset: pd.DataFrame,
     df_correlated_asset: Optional[List[pd.DataFrame]] = None,
-    df_indicators: Optional[List[pd.DataFrame]] = None,
+    df_indicators: Optional[pd.DataFrame] = None,
     target_field: str = "close",
     sequence_length: int = 60,
     to_generator: bool = True,
@@ -33,6 +33,9 @@ def trend_ts(
         Either an generator that generate tuple(target, label) or 2 numpy array in format (data, label)
 
     """
+    # Merge indicator datas
+    df_target_asset = pd.merge(df_target_asset, df_indicators, left_index=True, right_index=True, how='inner').dropna()
+
     # Get the trend target (long/short)
     df_target_asset["trend"] = (
         df_target_asset[target_field] <= df_target_asset[target_field].shift(-1)
@@ -40,13 +43,11 @@ def trend_ts(
     df_target_asset = df_target_asset[:-1]
 
     # Crop and match the timestamp
-    x = [df_target_asset] + [
-        item
-        for sublist in (df_correlated_asset, df_indicators)
-        if sublist is not None
-        for item in sublist
-    ]
-    x = process_timeframe(x)
+    if df_correlated_asset is not None:
+        x = [df_target_asset] + [*df_correlated_asset]
+        x = process_timeframe(x)
+    else:
+        x = process_timeframe([df_target_asset])
 
     # Gather the y (target)
     y = x[0]["trend"].to_numpy()
