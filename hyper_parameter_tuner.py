e4896f5f17bbcd242591875f6dde379f21ea0af0
qhuy0801 <qhuy0168@gmail.com>
2024-04-23 12:01:01 +0100

    :zap: Finalise hyper-param-tuner

--- hyper_parameter_tuner.py
+++ hyper_parameter_tuner.py
@@ -15,100 +15,88 @@ sweep_config = {
     "name": "QCOM_single_BBANDS_MACD",
     "method": "bayes",
     "metric": {
-        "name": "accuracy",
+        "name": "epoch/val_accuracy",
         "goal": "maximize",
     },
     "parameters": {
-        "sequence_length": {"values": [20, 40, 60]},
-        "feature_extracting_layer": {"values": [32, 64, 128]},
-        "lstm_layers": {"values": [[128, 64, 32], [64, 32, 16], [64, 32], [32, 16]]},
+        "sequence_length": {"values": [10, 20, 40, 60]},
+        "feature_extracting_layer": {"values": [64, 32, 16]},
+        "optimiser": {"values": ["adam", "sgd"]},
+        "lstm_layers": {
+            "values": [
+                [64, 64, 64],
+                [32, 32, 32],
+                [16, 16, 16],
+                [64, 64],
+                [32, 32],
+                [16, 16],
+                [64, 32, 16],
+                [64, 32],
+                [32, 16],
+            ]
+        },
         "lstm_l1": {"values": [None, 0.01, 0.02]},
-        "fc_layers": {"values": [[32, 16, 8], [32, 16], [16, 8]]},
+        "fc_layers": {
+            "values": [
+                None,
+                [64, 64],
+                [32, 32],
+                [16, 16],
+                [64, 32],
+                [32, 16],
+                [64],
+                [32],
+                [16],
+            ]
+        },
         "fc_l1": {"values": [None, 0.01, 0.02]},
         "fc_activation": {"values": ["tanh", "sigmoid"]},
-        "dropout": {"values": [None, 0.2, 0.4]},
+        "lstm_dropout": {"values": [None, 0.1, 0.2, 0.3, 0.4]},
+        "fc_dropout": {"values": [None, 0.1, 0.2, 0.3, 0.4]},
     },
 }
 
 
-def searching_train():
-    # Init the run
-    run = wandb.init(project="fin_timeseries")
-
-    # Get the configuration for instance
-    config = wandb.config
-
+def searching_train(config=None):
     # Date formatting
     date_format = "%Y-%m-%d"
 
-    train(
-        # General configuration
-        func="TIME_SERIES_INTRADAY",
-        interval="15min",
-        target_symbol="QCOM",
-        period=(
-            datetime.strptime("2019-01-01", date_format),
-            datetime.strptime("2024-01-01", date_format)
-        ),
-        correlated_symbols=["GOOGL"],
-        indicator_funcs=None,
-        batch_size=200,
-        to_generator=False,
-        model_name="trend_lstm",
-        sequence_length=config.sequence_length,
-        validation_size=0.2,
-        epochs=10,
-        wandb_log=run,
-        # Indicator configuration
-        time_period="20",
-        series_type="close",
-        nbdevup="1.5",
-        nbdevdn="1.5",
-        fastperiod="12",
-        slowperiod="26",
-        signalperiod="9",
-        # Model configuration
-        feature_extracting_layer=config.feature_extracting_layer,
-        lstm_layers=config.lstm_layers,
-        lstm_l1=config.lstm_l1,
-        fc_layers=config.fc_layers,
-        fc_l1=config.fc_l1,
-        fc_activation=config.fc_activation,
-        dropout=config.dropout,
-    )
-
-    # train(
-    #     # General configuration
-    #     func="TIME_SERIES_INTRADAY",
-    #     interval="15min",
-    #     target_symbol="QCOM",
-    #     period=None,
-    #     correlated_symbols=None,
-    #     indicator_funcs=None,
-    #     batch_size=200,
-    #     to_generator=False,
-    #     model_name="trend_lstm",
-    #     sequence_length=60,
-    #     validation_size=0.2,
-    #     epochs=10,
-    #     wandb_log=run,
-    #     # Indicator configuration
-    #     time_period="20",
-    #     series_type="close",
-    #     nbdevup="1.5",
-    #     nbdevdn="1.5",
-    #     fastperiod="12",
-    #     slowperiod="26",
-    #     signalperiod="9",
-    #     # Model configuration
-    #     feature_extracting_layer=32,
-    #     lstm_layers=[32, 32, 32],
-    #     lstm_l1=None,
-    #     fc_layers=None,
-    #     fc_l1=None,
-    #     fc_activation="tanh",
-    #     dropout=0.35,
-    # )
+    # Run
+    with wandb.init(config=config) as run:
+        config = wandb.config
+        train(
+            # General configuration
+            func="TIME_SERIES_INTRADAY",
+            interval="5min",
+            target_symbol="QCOM",
+            period=(
+                datetime.strptime("2019-04-01", date_format),
+                datetime.now(),
+            ),
+            # Indicator
+            indicator_settings={
+                "MACD": {},
+                "BBANDS": {},
+                "RSI": {}
+            },
+            to_generator=False,
+            batch_size=200,
+            model_name="trend_lstm",
+            sequence_length=config.sequence_length,
+            validation_size=0.3,
+            epochs=20,
+            wandb_log=run,
+            # Model configuration
+            feature_extracting_layer=config.feature_extracting_layer,
+            optimiser=config.optimiser,
+            lstm_layers=config.lstm_layers,
+            lstm_l1=config.lstm_l1,
+            fc_layers=config.fc_layers,
+            fc_l1=config.fc_l1,
+            fc_activation=config.fc_activation,
+            lstm_dropout=config.lstm_dropout,
+            fc_dropout=config.fc_dropout,
+        )
 
 
 if __name__ == "__main__":
@@ -117,5 +105,5 @@ if __name__ == "__main__":
 
     # Use Sweep to perform hyper-parameter tuning
     wandb.agent(
-        sweep_id="dqotfmuu", project="fin_timeseries", function=searching_train, count=20
+        sweep_id="blp1h38i", project="fin_timeseries", function=searching_train, count=20
     )
