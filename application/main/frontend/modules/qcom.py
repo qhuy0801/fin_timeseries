43a03b96ed910b5cf181ac66f204725c48c6504f
qhuy0801 <qhuy0168@gmail.com>
2024-04-26 14:46:30 +0100

    :sparkles: Streamlit QCOM _ searching iframe

---
+++ application/main/frontend/modules/qcom.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+with st.expander("Model selection"):
+    components.iframe(
+        "https://wandb.ai/qhuy0168/fin_timeseries/reports/LSTM-Model-selection-of-QCOM-on-30-mins-interval--Vmlldzo3NzExMDI3",
+        height=1024,
+        scrolling=True,
+    )
